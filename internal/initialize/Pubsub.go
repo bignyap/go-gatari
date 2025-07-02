6be0100775184dc6c57183b0053ba6540ec38d86
bpathi <bpathi@IN-BPATHI-M.local>
2025-07-02 20:50:51 +0530

    adding PUBSUB initializer

---
+++ internal/initialize/Pubsub.go
@@ -0,0 +1,37 @@
+package initialize
+
+import (
+	"os"
+	"strings"
+
+	"github.com/bignyap/go-utilities/pubsub"
+)
+
+func LoadPubSub() (pubsub.PubSubClient, error) {
+
+	pubCfg := pubsub.Config{
+		Type:      os.Getenv("PUBSUB_TYPE"),
+		Enabled:   os.Getenv("PUBSUB_ENABLED") == "true",
+		Namespace: os.Getenv("PUBSUB_NAMESPACE"),
+	}
+
+	switch pubCfg.Type {
+	case "redis":
+		pubCfg.Redis = &pubsub.RedisConfig{
+			URL: os.Getenv("REDIS_URL"),
+		}
+	case "kafka":
+		pubCfg.Kafka = &pubsub.KafkaConfig{
+			Brokers: strings.Split(os.Getenv("KAFKA_BROKERS"), ","),
+			GroupID: os.Getenv("KAFKA_GROUP_ID"),
+			Topic:   os.Getenv("KAFKA_TOPIC"),
+		}
+	case "rabbitmq":
+		pubCfg.RabbitMQ = &pubsub.RabbitMQConfig{
+			URL:       os.Getenv("RABBITMQ_URL"),
+			QueueName: os.Getenv("RABBITMQ_QUEUE"),
+		}
+	}
+
+	return pubsub.NewPubSub(pubCfg)
+}
