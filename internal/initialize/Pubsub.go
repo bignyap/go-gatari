7ff4e37511d810022c6696055c4c1daa441740e2
bpathi <bpathi@IN-BPATHI-M.local>
2025-07-02 21:31:59 +0530

    add the password to the redis pubsub client initialization

--- internal/initialize/Pubsub.go
+++ internal/initialize/Pubsub.go
@@ -18,7 +18,8 @@ func LoadPubSub() (pubsub.PubSubClient, error) {
 	switch pubCfg.Type {
 	case "redis":
 		pubCfg.Redis = &pubsub.RedisConfig{
-			URL: os.Getenv("REDIS_URL"),
+			URL:      os.Getenv("REDIS_URL"),
+			Password: os.Getenv("REDIS_PASSWORD"),
 		}
 	case "kafka":
 		pubCfg.Kafka = &pubsub.KafkaConfig{
