547145986e9b33eb59538c86b9a7d0b212e3f871
bpathi <bpathi@IN-BPATHI-M.local>
2025-07-21 01:23:48 +0530

    adding multiple api docs

---
+++ apidoc/config.js
@@ -0,0 +1,8 @@
+window.apiConfig = {
+    swaggerHeader: "GATARI Documentation",
+    title: "GATARI Documentation | Swagger UI",
+    swaggerJson: {
+        "GoAdmin": "apidoc/go-admin/swagger.yaml",
+        "GateKeeper": "apidoc/gate-keeper/swagger.yaml"
+    } 
+};
